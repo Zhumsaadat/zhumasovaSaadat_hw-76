9c27d09af1b209cc8a95c5a89f8c238f1f8b9d6e
Zhumashova Saadat <saadat9195@gmail.com>
2024-04-04 20:10:35 +0300

    написала серверную часть

--- backend/index.ts
+++ backend/index.ts
@@ -1,12 +1,21 @@
 import express from 'express';
+import cors from  'cors';
 import messagesRouter from './routes/messages';
+import fileDb from  './fileDb'
 
 const app = express();
+app.use(express.json());
 const port = 8000;
 
-
+app.use(cors());
 app.use('/messages', messagesRouter);
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
+const run = async () => {
+    await fileDb.init();
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
+}
+
+void run();
+
--- backend/routes/messages.ts
+++ backend/routes/messages.ts
@@ -1,13 +1,40 @@
 import express from 'express';
+import { MessageMutation } from '../types';
+import fileDb from '../fileDb';
 
 const messagesRouter  = express.Router();
 
 messagesRouter.get("/", async (req, res) => {
-    res.send('hello');
+    const queryDate = req.query.datetime as string;
+
+    if(queryDate) {
+        const date = new Date(queryDate);
+        if(!isNaN(date.getDate())) {
+            const messages = await fileDb.getMessages(queryDate);
+            return res.send(messages)
+        }else {
+            return res.status(400).send({"error": "Date not correct!"});
+        }
+    }
+
+    const messages = await  fileDb.getMessages();
+    res.send(messages);
+
 });
 
 messagesRouter.post("/", async (req, res) => {
-    res.send('hello post');
+    if (req.body.message || req.body.author) {
+        return res.status(400).send({"error": "Author amd message must be present in the request"});
+    }
+
+    const msgData: MessageMutation = {
+        message: req.body.message,
+        author: req.body.author,
+    }
+
+    const msg = await fileDb.addMessages(msgData);
+
+    res.send(msg);
 });
 
 
